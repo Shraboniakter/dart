2db8c55657d83b11b10be89c693c23cbec1ab835
Shraboniakter <110442617+Shraboniakter@users.noreply.github.com>
2023-08-02 22:15:51 +0600

    Add files via upload

---
+++ w.dart
@@ -0,0 +1,21 @@
+
+  void main() {
+  // Declare constant variables "a" and "b"
+  const int a = 10;
+  const int b = 3;
+
+  // Perform arithmetic operations
+  int addition = a + b;
+  int subtraction = a - b;
+  int multiplication = a * b;
+  double division = a / b; // Division may produce a non-integer result, so we use a double type.
+  int modulus = a % b;
+
+  // Print the results
+  print("Addition: $addition");
+  print("Subtraction: $subtraction");
+  print("Multiplication: $multiplication");
+  print("Division: $division");
+  print("Modulus: $modulus");
+}
+
