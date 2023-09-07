2aa5e55fb9426445acb1d9b2d2497af0ce309e00
shraboni akter <110442617+Shraboniakter@users.noreply.github.com>
2023-09-08 00:17:46 +0600

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyProject());
+}
+
+class MyProject extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyApp());
+  }
+
+}
+class MyApp extends StatelessWidget{
+  get childrent => null;
+
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      toolbarHeight: 100,
+      elevation: 70,
+      backgroundColor: Colors.green,
+      title:Center(
+        child:Text('Home'),),
+       titleTextStyle: TextStyle(fontSize:40,fontWeight:FontWeight.w700 ),
+       leading: Icon(
+         Icons.add_business_rounded,
+         size: 70,
+       ),
+       actions: [
+         IconButton(onPressed: (){}, icon: Icon(Icons.search),iconSize: 70,),
+
+       ],
+      ),
+    body: Column(
+      mainAxisAlignment:MainAxisAlignment.center,
+      children:[
+        Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:[
+              Text('This is mod 5 Assignment',
+              style:TextStyle(color: Colors.black,fontSize:20 ,fontWeight: FontWeight.w600),
+
+            ),
+            ]
+        ),
+
+        Row(
+          mainAxisAlignment:MainAxisAlignment.center,
+          children:[
+                      Text('MY ',style: TextStyle(fontSize: 30, color: Colors.redAccent),),
+                      Text('Phone ',style: TextStyle(fontSize: 20, color: Colors.lightBlueAccent),),
+                      Text('Name ',style: TextStyle(fontSize: 20.5, color: Colors.purple),),
+                      Text('is iphone 7',style: TextStyle(fontSize: 40, color: Colors.amber),),
+  ],
+        ),
+      ],
+    ),
+  );
+  }
+
+}
